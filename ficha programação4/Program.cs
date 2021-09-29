68f49d95ec82399d4bb158cc53f6f5e0ae754447
Adrian Gabriel <lycanboss1995@gmail.com>
2021-09-29 11:11:41 -0300

    Adicionar arquivos de projeto.

---
+++ ficha programação4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ficha_programação4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1415;
+            double a, r;
+            Console.WriteLine("Informe qual o valor do raio do círculo");
+            r = Double.Parse(Console.ReadLine());
+            a = pi * (Math.Pow(r, 2));
+            Console.WriteLine("O valor da área do círculo é:{0}", a);
+            Console.ReadLine();
+        }
+    }
+}
